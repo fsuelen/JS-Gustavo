c52cdf7e6b3d9735fc17c28a0ebaf6d4651df426
Suelen Silva <51544768+fsuelen@users.noreply.github.com>
2020-03-19 08:40:55 -0300

    tentando fazer o ultimo exercicio

--- projeto-js2/script.js
+++ projeto-js2/script.js
@@ -1,1 +1,19 @@
-alert("olá");
+function pegar() {
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let resultado = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('ERRO: Por Favor Preencha Todos os Campos')
+    } else {
+        resultado.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        for (let c = 1; c <= f; c += p) {
+            resultado.innerHTML += `${c}`
+        }
+
+    }
+}
